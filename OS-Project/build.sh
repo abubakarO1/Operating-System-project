0897e052a44cc4e467407d598f7b04ee8ac84804
Abubakar <133882280+abubakarO1@users.noreply.github.com>
2024-01-31 22:05:17 +0500

    done

---
+++ OS-Project/build.sh
@@ -0,0 +1,13 @@
+cores=$1
+ram=$(($2*1024))
+disk=$(($3*1024))
+
+echo $cores > osspecs
+echo $ram >> osspecs
+echo $disk >> osspecs
+
+g++ -o run run.cpp 
+g++ -o FantasyOS main.cpp -lpthread
+
+./run $cores $ram $disk
+./FantasyOS
